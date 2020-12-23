7fa3a1d4b377b0944f0dbf7981cffb713033ad63
Andreas Schuler <andreas.schuler@fh-hagenberg.at>
2020-12-23 18:56:47 +0100

    Sample for extracting data from the markdown file

---
+++ r4-core-master/_index.md
@@ -0,0 +1,2 @@
+# This is a content file
+lorem ipsum dolor sit amet
--- update.py
+++ update.py
@@ -1,5 +1,8 @@
 import markdown
+import glob
 
-html = markdown.markdown("#Header");
+# Read all directories and look for a index.md file
+# if found, the content of the file is added to the index.html in this repository.
 
-print(html)
+for name in glob.glob('./*/_index.md'):
+    print '\t', markdown.markdown(name);
