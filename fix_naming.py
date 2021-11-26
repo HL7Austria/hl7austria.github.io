4f299147171226c0dc47acc00ac48413bbfa3a0c
Andreas Schuler <andreas.schuler@fh-hagenberg.at>
2021-11-26 16:42:11 +0100

    workaround for naming problem in IG publisher see #5

---
+++ fix_naming.py
@@ -0,0 +1,14 @@
+import glob
+import yaml
+import re
+
+regex = '<td>(N\/A)<\/td>'
+pattern = re.compile( regex, re.MULTILINE | re.DOTALL)
+for name in glob.glob('./*/*-mappings.html'):
+    if 'r4-core-ballot_2021_01' not in name:
+        with open(name) as file:  
+            c_out = pattern.sub( 'n/a', file.read() )
+            print(c_out)
+        with open(name, "w") as html:
+            html.write( c_out )
+            html.close()
