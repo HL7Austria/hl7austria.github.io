b3c17603d07c3ce14c35b27aa60251111d05fed8
Andreas Schuler <andreas.schuler@fh-hagenberg.at>
2021-11-26 15:45:01 +0100

    Changed update.py to display different types of IGs.

--- update.py
+++ update.py
@@ -5,12 +5,22 @@ import re
 # Read all directories and look for a index.md file
 # if found, the content of the file is added to the index.html in this repository.
 
+def populate_html( fName, name, label, branch, version, description, published ):
+    return f"""<a href="{fName}/index.html" class="list-group-item list-group-item-action flex-column align-items-start">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-1">{name}<span style="margin-left: 8px;" class="label {label}">{branch}</span></h5>
+                                <span class="label label-info">{version}</span>                      
+                            </div>
+                            <p class="mb-1" style="margin-top:6px;">{description}</p>                    
+                            <small>Last published:</small> <span class="label label-default">{published}</span>
+                            </a>"""
 
 # (.*)<!--\s*@@begin-include
 # <!--\s*@@end-include\s*-->(.*)
 
 regex = '<!--\s*@@begin-include\s*-->(.*)<!--\s*@@end-include\s*-->'
 content = '<!-- @@begin-include -->'
+branchContent =  ''
 for name in glob.glob('./*/_index.yml'):
     
     with open(name) as file:  
@@ -21,17 +31,14 @@ for name in glob.glob('./*/_index.yml'):
         if index_yml[4].get("branch", "n.a.") == 'current':
             current = 'label-success'
         
-        content += f"""<a href="{folder_name}/index.html" class="list-group-item list-group-item-action flex-column align-items-start">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5 class="mb-1">{index_yml[0].get("name", "n.a.")}<span style="margin-left: 8px;" class="label {current}">{index_yml[4].get("branch", "n.a.")}</span></h5>
-                            <span class="label label-info">{index_yml[1].get("version", "n.a.")}</span>                      
-                        </div>
-                        <p class="mb-1" style="margin-top:6px;">{index_yml[2].get("description", "n.a.")}</p>                    
-                        <small>Last published:</small> <span class="label label-default">{index_yml[3].get("last_published", "n.a.")}</span>
-                        </a>"""   
+        tmp = populate_html(folder_name, index_yml[0].get("name", "n.a."), current, index_yml[4].get("branch", "n.a."), index_yml[1].get("version", "n.a."), index_yml[2].get("description", "n.a."), index_yml[3].get("last_published", "n.a."))  
+        if index_yml[5].get("type", "") == 'official':
+            content += tmp
+        else:
+            branchContent += tmp
 
 
-content =  content + '<!-- @@end-include -->'
+content =  content + '<h4 style="margin-top:20px">Working drafts</h4>' + branchContent + '<!-- @@end-include -->'
 with open('./index.html','r',encoding="utf8") as inputfile:
     pattern = re.compile( regex, re.MULTILINE | re.DOTALL)
     c_out = pattern.sub( content,  inputfile.read() )
