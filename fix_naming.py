ac5de063ec545344fbb4ae30a067afe1185cefdb
Andreas Schuler <andreas.schuler@fh-hagenberg.at>
2021-11-26 16:58:19 +0100

    changed regex - included td

--- fix_naming.py
+++ fix_naming.py
@@ -7,8 +7,7 @@ pattern = re.compile( regex, re.MULTILINE | re.DOTALL)
 for name in glob.glob('./*/*-mappings.html'):
     if 'r4-core-ballot_2021_01' not in name:
         with open(name) as file:  
-            c_out = pattern.sub( 'n/a', file.read() )
-            print(c_out)
+            c_out = pattern.sub( '<td>n/a</td>', file.read() )
         with open(name, "w") as html:
             html.write( c_out )
             html.close()
