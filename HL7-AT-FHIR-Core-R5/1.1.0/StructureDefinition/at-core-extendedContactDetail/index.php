8897fe72532c2aff194805d543f09fb0c6777fcf
gabriel0316 <gabriel0316@users.noreply.github.com>
2024-05-03 09:47:57 +0000

    Publish IG

---
+++ HL7-AT-FHIR-Core-R5/1.1.0/StructureDefinition/at-core-extendedContactDetail/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://hl7.at/fhir/HL7ATCoreProfiles/5.0.0/1.1.0/StructureDefinition-at-core-extendedContactDetail.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://hl7.at/fhir/HL7ATCoreProfiles/5.0.0/1.1.0/StructureDefinition-at-core-extendedContactDetail.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://hl7.at/fhir/HL7ATCoreProfiles/5.0.0/1.1.0/StructureDefinition-at-core-extendedContactDetail.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://hl7.at/fhir/HL7ATCoreProfiles/5.0.0/1.1.0/StructureDefinition-at-core-extendedContactDetail.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://hl7.at/fhir/HL7ATCoreProfiles/5.0.0/1.1.0/StructureDefinition-at-core-extendedContactDetail.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://hl7.at/fhir/HL7ATCoreProfiles/5.0.0/1.1.0/StructureDefinition-at-core-extendedContactDetail.html');
+else 
+  Redirect('http://hl7.at/fhir/HL7ATCoreProfiles/5.0.0/1.1.0/StructureDefinition-at-core-extendedContactDetail.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
