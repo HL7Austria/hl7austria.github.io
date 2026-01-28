c4c032a132598989701ad5935227f91dab0ee5e3
gabriel0316 <gabriel0316@users.noreply.github.com>
2026-01-28 10:23:56 +0000

    deploy: HL7Austria/ELGA-AustrianPatientSummary-R4@b704bf5785beb6c5bcd0e5077006345561059669

---
+++ r4-ELGA-AustrianPatientSummary-48-30-53-71---remove-ordered-and-closed-slicing-within-composition/scripts/tables/vitalsigns_config.js
@@ -0,0 +1,136 @@
+export const i_vitalsigns_config = {
+  layout: "fitDataStretch",
+  responsiveLayout: "collapse",
+  rowHeader: {
+    formatter: "responsiveCollapse",
+    hozAlign: "center",
+    resizable: false,
+    headerSort: false,
+    cssClass: "wrap-text",
+  },
+  responsiveLayoutCollapseStartOpen: false, 
+  columns: [
+    {
+      title: "",
+      field: "marked",
+      width: 55,
+      minWidth: 55,
+      hozAlign: "center",
+      formatter: function (cell) {
+        const data = cell.getRow().getData();
+        const symbol = data.marked === 1 ? '⚠️' : data.marked === 2 ? '?' : '▢';
+        return `
+          <span class="status-icon" style="cursor:pointer;">${symbol}</span>
+        `;
+      },
+      cellClick: function (e, cell) {
+        const rowData = cell.getRow().getData();
+        
+        if (rowData.marked === undefined) {
+          rowData.marked = 0; 
+        }
+        rowData.marked = (rowData.marked + 1) % 3;
+        cell.getRow().update(rowData);
+      },
+      responsive: 1,
+    },
+    {
+      title: "Zeitpunkt",
+      field: "zeitpunkt",
+      responsive: 3,
+        width: 150,
+        minWidth: 150,
+      cssClass: "wrap-text",
+    },
+    {
+      title: "Durchgeführter Test",
+      field: "test",
+      responsive: 0,
+      width: 300,
+      minWidth: 200,
+      cssClass: "wrap-text",
+    },
+    {
+      title: "Ergebnis",
+      field: "ergebnis",
+      responsive: 2,
+      width: 250,
+      minWidth: 200,
+      cssClass: "wrap-text",
+    },
+    {
+      title: "Codesystem<br>Test",
+      field: "codesystemTest",
+      formatter: function (cell) {
+        const row_data = cell.getRow().getData();
+        const codesystem_type_test = row_data?.codesystem_type_test;
+        const url = cell.getValue();
+        return url == '-' ? '-' : `<a href="${url}" target="_blank" class="a-explanation">${codesystem_type_test || url}</a>`
+      },
+      cssClass: "wrap-text",
+      responsive: 4,
+      width: 250,
+      minWidth: 150,
+      cssClass: "wrap-text",
+    },
+    {
+      title: "Codesystem<br>Ergebnis",
+      field: "codesystemErgebnis",
+      formatter: function (cell) {
+        const row_data = cell.getRow().getData();
+        const codesystem_type_ergebnis = row_data?.codesystem_type_ergebnis;
+        const url = cell.getValue();
+        return url == '-' ? '-' : `<a href="${url}" target="_blank" class="a-explanation">${codesystem_type_ergebnis || url}</a>`
+      },
+      cssClass: "wrap-text",
+      responsive: 5,
+      width: 250,
+      minWidth: 80,
+      cssClass: "wrap-text",
+    },
+    {
+      title: "Status",
+      field: "status",
+      responsive: 6,
+      minWidth: 80,
+      cssClass: "wrap-text",
+    },
+  ],
+};
+
+export const v_vitalsigns_config = {
+  layout: "fitDataStretch",
+  responsiveLayout: "collapse",
+  rowHeader: {
+    formatter: "responsiveCollapse",
+    hozAlign: "center",
+    resizable: false,
+    headerSort: false,
+    cssClass: "wrap-text",
+  },
+  responsiveLayoutCollapseStartOpen: false, 
+  columns: [
+    {
+      title: "Zeitpunkt",
+      field: "zeitpunkt",
+      responsive: 2,
+      minWidth: 150,
+      cssClass: "wrap-text",
+    },
+    {
+      title: "Durchgeführter Test",
+      field: "test",
+      responsive: 0,
+      width: 500,
+      minWidth: 300,
+      cssClass: "wrap-text",
+    },
+    {
+      title: "Ergebnis",
+      field: "ergebnis",
+      responsive: 1,
+      minWidth: 150,
+      cssClass: "wrap-text",
+    },
+  ],
+};
